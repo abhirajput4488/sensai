0e3067fcad8627ec44fd9ef462706700b395130d
abhirajpoot <abhirao274408@gmail.com>
2025-03-26 17:12:31 +0530

    ai-career-coach

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subset: ["latin"] });
 
@@ -26,6 +27,8 @@ export default function RootLayout({ children }) {
         >
           <Header/>
           <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+
           <footer className="bg-muted/50 py-12">
            <div className="container mx-auto px-4 text-center text">
               <p>Hi this is ai career ❤️❤️</p>
